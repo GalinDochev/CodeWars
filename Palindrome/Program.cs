d68972e3b1cd2d9af43e596014dfa1a85c89a4eb
GalinDochev <galin.dochev@abv.bg>
2024-07-02 13:23:53 +0300

    Palindromes

---
+++ Palindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Kata
+{
+    public static bool CheckIfPalindrome(string word)
+    {
+        return word.ToLower().SequenceEqual(word.ToLower().Reverse());
+
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Kata.CheckIfPalindrome("madam"));
+    }
+}
