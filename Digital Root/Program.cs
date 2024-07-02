8fca1bd81b758f239d3be1926f96a5826a5a2bbf
GalinDochev <galin.dochev@abv.bg>
2024-07-02 13:23:19 +0300

    Digital Root

---
+++ Digital Root/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Number
+{
+    public static int DigitalRoot(long n)
+    {
+        string numbers = n.ToString();
+        while (numbers.Length > 1)
+        {
+            long accumulatedNumber = 0;
+            foreach (var number in numbers)
+            {
+                accumulatedNumber += number - '0'; ;
+            }
+            if (accumulatedNumber >=10)
+            {
+                numbers=accumulatedNumber.ToString();
+                continue;
+            }
+            else
+            {
+                numbers = accumulatedNumber.ToString();
+                return int.Parse(numbers);
+            }
+            
+        }
+        return int.Parse(numbers);
+    }
+}
+
+public class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine(Number.DigitalRoot(493193));
+    }
+}
