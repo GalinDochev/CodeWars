76edbce83eee4980e25113ffbedc89f20ec3fed4
GalinDochev <galin.dochev@abv.bg>
2024-07-02 13:22:46 +0300

    Camel Casing Task

---
+++ CamelCasing/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Kata
+{
+    public static string BreakCamelCase(string str)
+    {
+        string strWithoutCamelCasing = string.Empty;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsUpper(str[i]))
+            {
+                strWithoutCamelCasing += " ";
+            }
+            strWithoutCamelCasing += str[i];
+        }
+        return strWithoutCamelCasing;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Kata.BreakCamelCase(""));
+    }
+}
