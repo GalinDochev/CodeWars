5c643636594995176d53e399b2fdd65b3c8a732d
GalinDochev <galin.dochev@abv.bg>
2024-07-09 13:10:00 +0300

    FlatLands

---
+++ FlatLands/Program.cs
@@ -0,0 +1,34 @@
+﻿public static class FairRations
+{
+
+
+    public static string FairRation(List<int> B)
+    {
+        int bread_given = 0;
+        for (int i = 0; i <= B.Count - 2; i++)
+        {
+            if (B[i] % 2 != 0)
+            {
+                B[i]++;
+                B[i + 1]++;
+                bread_given += 2;
+            }
+
+        }
+        if (B[B.Count - 1] % 2 == 0)
+        {
+            return bread_given.ToString();
+        }
+        else
+        {
+            return "NO";
+        }
+    }
+}
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(FairRations.FairRation(new List<int> { 1, 2 }));
+        }
+    }
